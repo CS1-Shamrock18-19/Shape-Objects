3aa3d68d6c931915bd1f884921bb13ca6499f115
Mr. Fleming <efleming@stpatrick.org>
2019-01-09 13:00:05 -0600

    initial upload

---
+++ shapeObj.js
@@ -0,0 +1,22 @@
+//Your code
+
+let Rectangle = {
+
+}
+
+let Square = {
+
+}
+
+let Circle = {
+
+}
+
+//Dont forget about box and sphere
+
+
+
+
+
+//------------------------------------------------
+//copy paste test code when you finish under here
